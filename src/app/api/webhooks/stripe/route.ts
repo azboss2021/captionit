00b77a2836ec10323d2e569100fa31060102fafb
Caleb Wilson <alexanderzyn1@gmail.com>
2024-04-09 19:39:19 -0700

    Don't change nothing

--- src/app/api/webhooks/stripe/route.ts
+++ src/app/api/webhooks/stripe/route.ts
@@ -7,8 +7,6 @@ import stripe from "stripe";
 export async function POST(request: Request) {
   const body = await request.text();
 
-  console.log(body);
-
   const sig = request.headers.get("stripe-signature") as string;
   const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET!;
 
